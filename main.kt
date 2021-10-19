a56582d57fac9ce34d302737446e00bac227e080
N1ckn1ght <50321432+N1ckn1ght@users.noreply.github.com>
2021-10-19 10:44:53 +0800

    Update main.kt

--- main.kt
+++ main.kt
@@ -6,14 +6,12 @@ fun main()
     var n: Int = sc.nextInt()
     var m: Int = sc.nextInt()
 
-    val childs = LongArray(n + m) {0}
-    var psum: Long = 0
+    val rbits = LongArray(n + m) {0}
+    rbits[m - 1] = 1
 
-    childs[m] = 1
     for (i in (m + 1)..(m + n - 1))
     {
-        psum += childs[i - 2] - childs[i - m - 1]
-        childs[i] = psum
+        rbits[i] = rbits[i - 1] + rbits[i - 2] - rbits[i - m - 1]
     }
-    println((childs[n + m - 1] + psum + childs[n + m - 2] - childs[n - 1]).toString())
+    println((rbits[n + m - 1] + rbits[n + m - 2]).toString())
 }
